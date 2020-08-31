4647471342d6c7c024f5e26f30fb2dabb3e9b4ba
bumbum9944 <tong940526@gmail.com>
2020-08-31 15:21:40 +0900

    project dir url.py 수정 app views import 경로 수정했습니다.

--- git_practice/urls.py
+++ git_practice/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from git_practice import views
+from branch_practice import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
