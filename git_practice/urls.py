676ac017bc1e8b5bf15ffb36a4dd08235164cf24
bumbum9944 <tong940526@gmail.com>
2020-08-31 15:24:06 +0900

    기범 page url 추가

--- git_practice/urls.py
+++ git_practice/urls.py
@@ -20,4 +20,5 @@ from branch_practice import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
+    path('gibeom/', views.gibeom, name="gibeom")
 ]
