2c5176c941ace5e28c5d368ef7744bf0062a8ad0
cmin427@likelion.org <cmin427@gmail.com>
2020-08-31 15:25:30 +0900

    15:25

--- branch_practice/views.py
+++ branch_practice/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def home(requests):
     return render(requests, 'home.html')
+
+
+def cmin(requests):
+    return render(requests, 'cmin.html')
